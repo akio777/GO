55263dca0ec72eb0783c75629f24bd9f7cb53113
the next hokage <chanutpong.promsin@gmail.com>
2021-03-11 14:18:10 +0700

    ARAI NEER

---
+++ go.mod
@@ -0,0 +1,14 @@
+module GO
+
+go 1.15
+
+require (
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/labstack/echo/v4 v4.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
+)
--- server.go
+++ server.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
 	"net/http"
+	"os"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+//type Recorder interface{
+//	Bind(string, User) Recorder
+//	Interface() interface{}  // Get the struct that has been linked
+//	Insert() error // INSERT just one record
+//	Update() error // UPDATE just one record
+//	Delete() error // DELETE just one record
+//	Exists() (bool, error) // Check for just one record
+//	ExistsWhere(cond interface{}, args ...interface{}) (bool, error)
+//	Load() error  // SELECT just one record
+//	LoadWhere(cond interface{}, args ...interface{}) error // Alternate Load()
+//}
+
+
+
+type User struct{
+	id int `stbl:"id, AUTO_INCREMENT"`
+	username string `stbl:"username, PRIMARY_KEY"`
+	password string `stbl:"password"`
+}
+
 
 func MyMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
@@ -19,14 +45,49 @@ func MyMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 }
 
 func main() {
-	// Echo instance
-	e := echo.New()
 
+	if _, err:=os.Stat("./database/Authen.db"); err == nil{
+		log.Println("	:	Database exits.")
+		db, err := sql.Open("sqlite3", "./database/Authen.db")
+		if err != nil{
+			log.Fatal(err.Error())
+		}
+		_, err = db.Exec("create database if not exits user_info")
+		if err != nil{
+			log.Fatal(err.Error())
+		}
+
+		//else{
+		//	log.Println("found table")
+		//}
+
+	}else if os.IsNotExist(err) {
+		log.Println("database not found")
+		file, err := os.Create("./database/Authen.db")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		file.Close()
+		log.Println("Authen.db created")
+	} else {
+		log.Println("ERROR")
+	}
+
+	//db, err := sql.Open("sqlite3", "./database/Authen.db")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer db.Close()
+	// Echo instance
+
+	e := echo.New()
+	e.HideBanner = true
 	// Middleware
-	//e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	//	Format: "method=${method} from : ${remote_ip}, uri=${uri}, status=${status}\n",
-	//}))
-	//e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+	e.Use(middleware.Recover())
 
 	// Routes
 	e.GET("/", hello, MyMiddleware)
